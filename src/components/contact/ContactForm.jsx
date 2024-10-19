9f3a7779e8c16a58776bb5e8a233a5225e3ac805
name <email@gmail.com>
2024-10-19 18:11:49 +0300

    env

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -68,7 +68,7 @@ const ContactForm = () => {
           className="max-w-xl m-4 p-6 sm:p-10 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
         >
           <p className="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8">
-            Contact Form
+            Contact Us Form
           </p>
           <div className='flex flex-row justify-between gap-2 items-center'>
             <div className="font-general-regular mb-4">
